803fe1deb01db328152039df8736685f64f85b56
LeaClover <plesen_14@mail.ru>
2022-07-06 20:31:37 +0300

    add project task1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+// 4  -> 24
+// 5  -> 120
+
+namespace HelloWorld 
+{ 
+    class Program 
+    { 
+        public static void Main(string[] args) 
+        { 
+            Console.WriteLine("Введите число N и мы найдем произведение от 1 до N!");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int result = 1;
+            for (int i = 1; i <= N; i++)
+            {
+                result = result * i;
+            }
+            Console.WriteLine(result);
+        } 
+    } 
+}
